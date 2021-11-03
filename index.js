ab3a205a62da0e15a26b4031ba06aeed8a8fa7a2
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-11-03 20:57:02 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('hello');
+function preview(token){
+    $.getJSON("//ulogin.ru/token.php?host=" + encodeURIComponent(location.toString()) + "&token=" + token + "&callback=?", function(data){
+        data = $.parseJSON(data.toString());
+        if(!data.error){
+            alert("Привет, "+data.first_name+" "+data.last_name+"!");
+        }
+    });
+}

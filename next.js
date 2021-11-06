aae78a82294f965c79ac39a9bfb5b94b680e7101
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-11-07 02:56:17 +0300

    Update next.js

--- next.js
+++ next.js
@@ -1,7 +1,7 @@
 [...document.querySelectorAll('button')].forEach(function(item) {
     item.addEventListener('click', function() {
       console.log(item.innerHTML);
-      let b = JSON.parse(localStorage.getItem(values));
+      let b = JSON.parse(localStorage.getItem('values'));
       if(item.innerHTML == 'bro'){
         localStorage.setItem('bro', b[b.length-1])
       }

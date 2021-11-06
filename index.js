db2efc8db92861440ff6ef94bfae67188446b9a7
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-11-07 02:25:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 console.log(alasql("SELECT * FROM test"));
+let fn = [];
 function preview(token){
     $.getJSON("//ulogin.ru/token.php?host=" + encodeURIComponent(location.toString()) + "&token=" + token + "&callback=?", function(data){
         data = $.parseJSON(data.toString());
         if(!data.error){
-            localStorage.setItem('first_name', data.first_name);
-            localStorage.setItem('last_name', data.last_name);
-            localStorage.setItem('email', data.email);
-            localStorage.setItem('country', data.country);
+            if (localStorage.getItem('values') !== null){
+                c = localStorage.getItem('values');
+                c = JSON.parse(c);
+                console.log(c);
+                fn = c;
+                fn.push({
+                    first_name: data.first_name,
+                    last_name: data.last_name,
+                    email: data.email,
+                    country: data.country,
+                })
+                localStorage.setItem('values', fn);
+            }
+            else{
+                fn.push({
+                    first_name: data.first_name,
+                    last_name: data.last_name,
+                    email: data.email,
+                    country: data.country,
+                })
+                localStorage.setItem('values', fn);
+            }
+//             localStorage.setItem('first_name', data.first_name);
+//             localStorage.setItem('last_name', data.last_name);
+//             localStorage.setItem('email', data.email);
+//             localStorage.setItem('country', data.country);
             alert("Привет, "+data.first_name+" "+data.last_name+"!");
-            console.log(data);
+//             console.log(data);
         }
     });
     window.location.href = 'next.html';

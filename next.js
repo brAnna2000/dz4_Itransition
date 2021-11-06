a831fbaf2f4c1b55503486035973b0f912296e36
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-11-06 20:01:03 +0300

    Update next.js

--- next.js
+++ next.js
@@ -1,7 +1,8 @@
 let buttons = document.getElementsByClassName('button');
 // buttons.addEventListener('click', preview);
-
+let data = localStorage.getItem('first_name');
+let lastName = localStorage.getItem('last_name');
 // [...document.querySelectorAll('button')].forEach(function(item) {
 //     item.addEventListener('click', sending);
 // });
-console.log(data);
+console.log(data, lastName);

413c8ab64735d37fcffd3b36b3779429620a6639
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-11-07 02:54:53 +0300

    Update next.js

--- next.js
+++ next.js
@@ -1,5 +1,12 @@
 [...document.querySelectorAll('button')].forEach(function(item) {
     item.addEventListener('click', function() {
       console.log(item.innerHTML);
+      let b = JSON.parse(localStorage.getItem(values));
+      if(item.innerHTML == 'bro'){
+        localStorage.setItem('bro', b[b.length-1])
+      }
+      else{
+        localStorage.setItem('sis', b[b.length-1])  
+      }
     });
 });

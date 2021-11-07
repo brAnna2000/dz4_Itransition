d0ecf284164972c0a0bbfa1a7baecba1199b10c9
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-11-07 13:47:28 +0300

    Update next.js

--- next.js
+++ next.js
@@ -1,18 +1,17 @@
 let text1 = document.getElementById('text1');
 let text2 = document.getElementById('text2');
-console.log(JSON.parse(localStorage.getItem('bro')))
+
 if (localStorage.getItem('bro') !== null){
-    console.log(JSON.parse(localStorage.getItem('bro')));
     text1.textContent = "Sent by "+ JSON.parse(localStorage.getItem('bro')) + " at " + JSON.parse(localStorage.getItem('timeBro'));
 }
 else if (localStorage.getItem('sis') !== null){
-    console.log(JSON.parse(localStorage.getItem('sis')))
     text1.textContent = "Sent by "+ JSON.parse(localStorage.getItem('sis')) + " at " + JSON.parse(localStorage.getItem('timeSis'));
 }
 [...document.querySelectorAll('button')].forEach(function(item) {
     item.addEventListener('click', function() {
       let b = JSON.parse(localStorage.getItem('values'));
       let time = new Date();
+        console.log(item);
       if(item.innerHTML == 'bro'){
         localStorage.setItem('bro', JSON.stringify(b[b.length-1].first_name));
         localStorage.setItem('timeBro', JSON.stringify(time.getHours() + ':' + time.getMinutes()));

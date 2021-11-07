fcff6214065072e46b773918fa70addcfda6ca92
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-11-07 16:57:37 +0300

    Update message.js

--- message.js
+++ message.js
@@ -2,10 +2,10 @@ let author = document.getElementById('author');
 let message = document.getElementById('message');
 console.log(localStorage.getItem('broClick'));
 if(localStorage.getItem('broClick')==1){
-    author.textContent = JSON.parse(localStorage.getItem('bro')) + " " + JSON.parse(localStorage.getItem('broSurname')) + ' at ' + JSON.parse(localStorage.getItem('timeBro'));
+    author.textContent = localStorage.getItem('bro') + " " + localStorage.getItem('broSurname') + ' at ' + JSON.parse(localStorage.getItem('timeBro'));
     message.textContent = "Bro!"; 
 }
 else{
-    author.textContent = JSON.parse(localStorage.getItem('sis')) + " " + JSON.parse(localStorage.getItem('sisSurname')) + ' at ' + JSON.parse(localStorage.getItem('timeSis'));
+    author.textContent = localStorage.getItem('sis') + " " + localStorage.getItem('sisSurname') + ' at ' + JSON.parse(localStorage.getItem('timeSis'));
     message.textContent = "Bro!";
 }

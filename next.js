1f9478ac373dae1230fd7fbafb6c90f6bcb2fa3a
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-11-07 03:05:50 +0300

    Update next.js

--- next.js
+++ next.js
@@ -1,11 +1,14 @@
 [...document.querySelectorAll('button')].forEach(function(item) {
     item.addEventListener('click', function() {
       let b = JSON.parse(localStorage.getItem('values'));
+      let time = new Date();
       if(item.innerHTML == 'bro'){
-        localStorage.setItem('bro', JSON.stringify(b[b.length-1].first_name))
+        localStorage.setItem('bro', JSON.stringify(b[b.length-1].first_name));
+        localStorage.setItem('time', JSON.stringify(time.getHours + ':' + time.getMinutes));
       }
       else{
-        localStorage.setItem('sis', JSON.stringify(b[b.length-1].first_name))  
+        localStorage.setItem('sis', JSON.stringify(b[b.length-1].first_name));
+        localStorage.setItem('time', JSON.stringify(time.getHours + ':' + time.getMinutes));  
       }
     });
 });

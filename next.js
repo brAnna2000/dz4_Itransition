2ea6a6c51a5e56c134a9a32b8123631e099c885f
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-11-07 13:40:27 +0300

    Update next.js

--- next.js
+++ next.js
@@ -2,12 +2,12 @@ let text1 = document.getElementById('text1');
 let text2 = document.getElementById('text2');
 console.log(JSON.parse(localStorage.getItem('bro')))
 if (localStorage.getItem('bro') !== null){
-    console.log(JSON.parse(localStorage.getItem('bro')))
-    text1.textContent = JSON.parse(localStorage.getItem('bro'));
+    console.log(JSON.parse(localStorage.getItem('bro')));
+    text1.textContent = "Sent by "+ JSON.parse(localStorage.getItem('bro')) + "at " + JSON.parse(localStorage.getItem('timeBro'));
 }
 else if (localStorage.getItem('sis') !== null){
     console.log(JSON.parse(localStorage.getItem('sis')))
-    text2.textContent = JSON.parse(localStorage.getItem('sis'));
+    text1.textContent = "Sent by "+ JSON.parse(localStorage.getItem('sis')) + "at " + JSON.parse(localStorage.getItem('timeSis'));
 }
 [...document.querySelectorAll('button')].forEach(function(item) {
     item.addEventListener('click', function() {
@@ -15,11 +15,11 @@ else if (localStorage.getItem('sis') !== null){
       let time = new Date();
       if(item.innerHTML == 'bro'){
         localStorage.setItem('bro', JSON.stringify(b[b.length-1].first_name));
-        localStorage.setItem('time', JSON.stringify(time.getHours() + ':' + time.getMinutes()));
+        localStorage.setItem('timeBro', JSON.stringify(time.getHours() + ':' + time.getMinutes()));
       }
       else{
         localStorage.setItem('sis', JSON.stringify(b[b.length-1].first_name));
-        localStorage.setItem('time', JSON.stringify(time.getHours() + ':' + time.getMinutes()));  
+        localStorage.setItem('timeSis', JSON.stringify(time.getHours() + ':' + time.getMinutes()));  
       }
     });
 });

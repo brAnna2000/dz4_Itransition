9234ec834b5de53d5a3176836847f76f2c40f95c
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-11-07 03:27:03 +0300

    Update next.js

--- next.js
+++ next.js
@@ -5,7 +5,7 @@ if (localStorage.getItem('bro') !== null){
     text1.textContent = JSON.parse(localStorage.getItem('bro'));
 }
 if (localStorage.getItem('sis') !== null){
-    text1.textContent = JSON.parse(localStorage.getItem('sis'));
+    text2.textContent = JSON.parse(localStorage.getItem('sis'));
 }
 [...document.querySelectorAll('button')].forEach(function(item) {
     item.addEventListener('click', function() {
